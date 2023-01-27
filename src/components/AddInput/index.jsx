42f7697c08b20dde93ae70fea28af7bda53c841e
gunawansetia <newgunawansetiawiguna@gmail.com>
2023-01-27 21:14:41 +0700

    Create component css button

--- src/components/AddInput/index.jsx
+++ src/components/AddInput/index.jsx
@@ -1,14 +1,35 @@
 import styled from "@mui/styled-engine";
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+
+import styles from "./AddInput.module.css";
 
 const CssTextField = styled(TextField)({
+  width: "60vw",
   "& .MuiOutlinedInput-root": {
     borderRadius: "5px",
   },
 });
 
+const CssButton = styled(Button)({
+  backgroundColor: "#3886F7",
+  padding: "10px 30px",
+  borderRadius: "10px",
+  margin: "0 20px",
+  color: "#FFFFFF",
+  "&:hover": {
+    backgroundColor: "#227BFB",
+  },
+});
+
 const AddInput = () => {
-  return <CssTextField fullWidth label="Add The Task..." id="add-task" />;
+  return (
+    <>
+      <div className={styles.addInput}>
+        <CssTextField label="Add The Task..." id="add-task" />{" "}
+        <CssButton>Add</CssButton>
+      </div>
+    </>
+  );
 };
 
 export default AddInput;
